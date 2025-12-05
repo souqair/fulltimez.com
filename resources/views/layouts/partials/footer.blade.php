2f84f245e27ef7ba565f925a98ce6a0e56a5777e
intiahmad91 <intiahmad91@gmail.com>
2025-12-05 23:21:38 +0500

    Update footer.blade.php

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,42 +1,4 @@
-<section class="section download">
-      <div class="container">
-         <div class="row">
-            <div class="col-md-6 col-12">
-               <div class="section__header">
-                  <h2 class="section__title">Download <span>FULLTIMEZ's</span> <br class="d-none d-md-block"> mobile app for
-                     Free</h2>
-                  <p class="section__desc">
 
-                     FullTimez, empowering your career journey. Whether you're searching for your first job or
-your next big opportunity, we connect you with employers who value your talent. Explore
-thousands of jobs, get expert career advice, and take the next step toward a brighter future —
-all in one place.
-Helping you move forward in your career. Our platform is designed to support jobseekers by
-providing access to job listings, career resources, and guidance tools. Please note that we do
-not guarantee employment or endorse any specific employer. Always exercise due diligence
-when applying and communicating with potential employers.
-
-                  </p>
-
-                  <div class="download__actions">
-                            <a href="#">
-   <img title="e-Invoice KSA iOS mobile APP" src="{{asset('images/apple.webp')}}" class="lazyload" alt="apple mobile accounting APP"></a>
-<a href="#"> <img title="e-Invoice KSA andriod mobile APP" alt="andriod mobile accounting APP" src="{{asset('images/google-play.jpg')}}" class="lazyload"></a>
-<a href="#" style="margin-top:5px"> <img title="e-Invoice KSA Huawei mobile APP" alt="Mobile Accounting APP Huawei " src="{{asset('images/huawei.webp')}}" class="lazyload"></a>
-</div>
-                
-                         
-               </div>
-            </div>
-
-            <div class="col-md-6 col-12">
-            <div class="mobileapp text-center"><img src="{{asset('images/mobileapp.png')}}" alt=""></div>
-         </div>
-             
-         </div>
-      </div>
-   </section>
-<div class="ads_ text-center py-5"><img src="images/ads2.jpg" alt=""></div>
 
 
 <style>
