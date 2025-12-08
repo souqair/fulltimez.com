fbee5152d4fd448a16cdba86235b031dcda1a48a
intiahmad91 <intiahmad91@gmail.com>
2025-12-08 22:19:19 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}?v={{ time() }}" rel="stylesheet">
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('css/fontawesome.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('css/owl.carousel.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.css') }}?v={{ time() }}" rel="stylesheet">
     <link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/newfancybox.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom-fixes.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/responsive-improvements.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/newfancybox.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('css/custom-fixes.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive-improvements.css') }}?v={{ time() }}" rel="stylesheet">
     <style>
         .dashboard_wrap {
             background: #f9f9ff !important;
